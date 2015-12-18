09f4268d4c017fa6924bb829c8d862af6178ce16
Timothy Gregg <timtgregg@gmail.com>
2015-12-18 12:57:02 -0500

    starting fri

--- js/songs/index.controller.js
+++ js/songs/index.controller.js
@@ -5,10 +5,17 @@
   .module("songs")
   .controller("SongsIndexController", [
     "SongsFactory",
+    "$sce",
     SongsIndexControllerFunction
   ]);
 
   function SongsIndexControllerFunction(SongsFactory){
     this.songs = SongsFactory.query();
+    this.newSong = new SongsFactory();
+    this.play = function(song, $sce){
+      this.playSong = song;
+      // workaround for ng-src not allowing expression {{song.audio_url}}
+      this.playSong.encodedUrl = $sce.trustAsResourceUrl(song.audio_url);
+    };
   }
 }());
